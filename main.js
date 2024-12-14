7785155681a2e2438dbadded18142937a1618df9
LogLot <prattlog3@gmail.com>
2024-12-14 13:08:59 -0500

    fixed joystick tester

--- main.js
+++ main.js
@@ -17,26 +17,29 @@ function tick(){
     ctx.rect(0,0,10000,10000)
     ctx.fillStyle="rgb(167,199,216)"
     ctx.fill()
-    var x = 500
-    var y = 300
-    var r = 20
+    var x = 2514/2
+    var y = 1377/2
+    var r = 70
+    var rChange=7
+    var dist=150
+    var activeRSub = 20
     let gamepad = [];
     if (navigator.getGamepads) gamepad = navigator.getGamepads();
     else if (navigator.webkitGetGamepads) gamepads = navigator.webkitGetGamepads();
     if(gamepad[0]!=null){
-        var xSet=Math.abs(gamepad[0].axes[0])>=.1 ? gamepad[0].axes[0]*100+x :0+x
-        var ySet=Math.abs(gamepad[0].axes[1])>=.1 ? gamepad[0].axes[1]*100+y :0+y
-        draw.Circle(xSet,ySet,r+4,"#33363f")
-        draw.Circle(xSet,ySet,r,"#afafbf")
+        var xSet=Math.abs(gamepad[0].axes[0])+Math.abs(gamepad[0].axes[1])>=.1 ? gamepad[0].axes[0]*dist+x :0+x
+        var ySet=Math.abs(gamepad[0].axes[0])+Math.abs(gamepad[0].axes[1])>=.1 ? gamepad[0].axes[1]*dist+y :0+y
+        draw.Circle(xSet,ySet,r+rChange-activeRSub,"#33363f")
+        draw.Circle(xSet,ySet,r-activeRSub,"#afafbf")
 
 
-        var xSet=Math.abs(gamepad[0].axes[2])>=.1 ? gamepad[0].axes[2]*100+x :0+x
-        var ySet=Math.abs(gamepad[0].axes[3])>=.1 ? gamepad[0].axes[3]*100+y :0+y
-        draw.Circle(xSet,ySet,r+4,"#33363f")
-        draw.Circle(xSet,ySet,r,"#bfafaf")
+        var xSet=Math.abs(gamepad[0].axes[2])+Math.abs(gamepad[0].axes[3])>=.1 ? gamepad[0].axes[2]*dist+x :0+x
+        var ySet=Math.abs(gamepad[0].axes[2])+Math.abs(gamepad[0].axes[3])>=.1 ? gamepad[0].axes[3]*dist+y :0+y
+        draw.Circle(xSet,ySet,r+rChange-activeRSub,"#33363f")
+        draw.Circle(xSet,ySet,r-activeRSub,"#bfafaf")
     
     }
-    draw.Circle(x,y,r+4,"#33363f")
+    draw.Circle(x,y,r+rChange,"#33363f")
     draw.Circle(x,y,r,"#afbfaf")
 
     requestAnimationFrame(tick)
